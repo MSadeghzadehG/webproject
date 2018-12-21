ff3739c20c81a2745030270528a730711ff39080
Mahdi Sadeghzadeh Ghamsary <msg_pc@MSadeghG.local>
2018-12-22 02:28:46 +0330

    use mongodb and test register function

--- node/f.js
+++ node/f.js
@@ -8,6 +8,21 @@ var fs = require('fs')
 var express = require('express')
 var compression = require('compression')
 
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017");
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('connected!')
+});
+
+var nameSchema = new mongoose.Schema({
+    firstName: String,
+    lastNameName: String
+});
+var User = mongoose.model("User", nameSchema);
+
 var app = express()
 var port = 5000
 
@@ -20,6 +35,17 @@ app.get('/' ,function (req,res){
     console.log(req)
     res.setHeader('Content-Type','text/html')
     res.sendFile('static/index.html')
+})
+
+app.get('/register',function (req,res){
+    var myData = new User({firstName:"first",lastNameName:"last"});//req.body);
+    myData.save()
+    .then(item => {
+    res.send("item saved to database");
+    })
+    .catch(err => {
+    res.status(400).send("unable to save to database");
+    });
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
