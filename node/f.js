8871d3d1962402ea4fbb3ed5c25e7086a4669928
Mahdi Sadeghzadeh Ghamsary <msg_pc@MSadeghG.local>
2018-12-22 03:41:29 +0330

    simple login check added

--- node/f.js
+++ node/f.js
@@ -18,8 +18,8 @@ db.once('open', function() {
 });
 
 var nameSchema = new mongoose.Schema({
-    firstName: String,
-    lastNameName: String
+    username: String,
+    password: String
 });
 var User = mongoose.model("User", nameSchema);
 
@@ -29,23 +29,62 @@ var port = 5000
 app.use(compression())
 
 // serves files in static folder
-
 app.use(express.static('static'))
+
 app.get('/' ,function (req,res){
     console.log(req)
     res.setHeader('Content-Type','text/html')
     res.sendFile('static/index.html')
 })
 
-app.get('/register',function (req,res){
-    var myData = new User({firstName:"first",lastNameName:"last"});//req.body);
-    myData.save()
-    .then(item => {
-    res.send("item saved to database");
-    })
-    .catch(err => {
-    res.status(400).send("unable to save to database");
-    });
+
+function check_username_exist(req,to_check){
+    var output = false
+    if (to_check=="register")
+        User.findOne({ username:req.query.username},function(err,user){
+            if (err){ 
+                console.log('error')
+            } else if (user) {
+                // console.log('true')
+                output = true
+            }
+            // console.log('false')
+        });
+    else if (to_check=="login")
+        User.findOne({ username:req.query.username,password:req.query.password},function(err,user){
+            if (err){ 
+                console.log('error')
+            } else if (user) {
+                // console.log('true')
+                output = true
+            }
+            // console.log('false')
+        });
+    return output
+}
+
+app.get('/register',function (req,res){ 
+    console.log(req.query)
+    try {
+        if (check_username_exist(req,"register")){
+            var myData = new User({username:req.query.username,password:req.query.password});//req.body);
+            myData.save()
+            console.log('done')
+            res.send(true);
+        } else 
+            res.send(false)
+    }
+    catch(err){
+        console.log('error')
+        res.send(false);
+    }
+    res.end()
+})
+
+app.get('/login', function(req,res){
+    console.log(req.query)
+    res.send(check_username_exist(req,"login"))
+    res.end()
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
